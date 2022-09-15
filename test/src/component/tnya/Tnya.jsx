5b81d93b50a9ee14fcbd5b9ba85dcffba51062e6
Spase338 <vladhastik@gmail.com>
2022-09-15 17:40:43 +0300

    Signed-off-by: Spase338 <vladhastik@gmail.com>

---
+++ test/src/component/tnya/Tnya.jsx
@@ -0,0 +1,12 @@
+import './Tnya.css'
+
+function Tnya() {
+	return (
+		<div className="box">
+			<div className="tnya siski"></div>
+			<div className="tnya jopa "></div>
+		</div>
+	)
+}
+
+export default Tnya
---
+++ test/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Tnya from './component/tnya/Tnya'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Tnya/>
+  </React.StrictMode>
+);
+
